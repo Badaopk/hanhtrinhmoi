6f1e221f2e925d012f4700c123755d0ed532553f
Badaopk <dienp3030@gmail.com>
2026-02-19 11:40:04 +0700

    Nâng cấp 21

--- server.js
+++ server.js
@@ -1954,9 +1954,7 @@ async function autoStartTourneyLogic() {
 setInterval(autoStartTourneyLogic, 60000);
 // Thêm tham số '0.0.0.0' để lắng nghe trên mọi giao diện mạng
 // Nếu có cổng do Render cấp thì dùng, không thì mặc định là 3000
-const PORT = process.env.PORT || 3000; 
 const HOST = '0.0.0.0'; 
-
 server.listen(PORT, HOST, () => {
     console.log(`🚀 Server đang chạy!`);
     console.log(`🏠 Local: http://localhost:3000`);
